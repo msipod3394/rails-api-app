5d5b9e7893a9c701267f19572c26aa0a6c946f7f
msipod3394 <msipod3394@gmail.com>
2024-03-17 10:39:55 +0900

    Co-authored-by: KeitaKageyama <KeitaKageyama@users.noreply.github.com>

--- db/migrate/20240317004807_add_foreign_key_to_orders_retry.rb
+++ db/migrate/20240317004807_add_foreign_key_to_orders_retry.rb
@@ -2,10 +2,10 @@ class AddForeignKeyToOrdersRetry < ActiveRecord::Migration[7.0]
   def change
 
     # 既存の外部キー制約を削除
-    # remove_foreign_key :orders, :users
+    remove_foreign_key :orders, :users
 
     # 新しい外部キー制約を追加（オプション変更）
-    # add_foreign_key :orders, :users, on_delete: :cascade
+    add_foreign_key :orders, :users, on_delete: :cascade
 
   end
 end
