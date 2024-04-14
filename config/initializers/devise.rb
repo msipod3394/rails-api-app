3797bfeac64d18214ce6ad13bb9aef795a30082d
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-14 09:09:59 +0900

    認証確認できた！

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,9 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.secret_key_base
+  end
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -264,6 +267,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.navigational_formats = [:html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
