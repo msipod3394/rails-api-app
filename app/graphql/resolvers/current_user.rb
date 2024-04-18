20308d339fbd843844931f820c415467defc711f
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-18 19:18:22 +0900

    修正

--- app/graphql/resolvers/current_user.rb
+++ app/graphql/resolvers/current_user.rb
@@ -1,6 +1,6 @@
 module Resolvers
   class CurrentUser < Resolvers::BaseQuery
-    type ObjectTypes::UserType, null: false
+    type Types::UserType, null: false
 
     def resolve
       context[:current_user]
