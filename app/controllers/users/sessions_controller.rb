e32ecd3a802f78958ee194805c5f544faa2c7ff0
msipod3394 <msipod3394@gmail.com>
2024-04-18 10:38:55 +0900

    修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,22 +4,13 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    if resource.persisted?
-      render json: { message: 'You are logged in.', user: resource }, status: :ok
-    else
-      invalid_login_attempt
-    end
+    render json: { message: 'You are logged in.', user: resource }, status: :ok
   end
-
 
   def respond_to_on_destroy
     log_out_success && return if current_user
 
     log_out_failure
-  end
-
-  def invalid_login_attempt
-    render json: { message: "ユーザー登録がありません" }, status: :unauthorized
   end
 
   def log_out_success
