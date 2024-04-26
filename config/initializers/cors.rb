c6bfadd6c90897c16824a697540bbddd066f7914
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-26 16:43:01 +0900

    corsエラー

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    # origins "http://localhost:3000"
+    origins "https://donmaru-app-new.vercel.app"
 
     resource "*",
       headers: :any,
